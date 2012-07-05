58e43e65ea59821911247437105711c3594959fe
Klaus Silveira <contact@klaussilveira.com>
2012-07-05 13:04:35 -0300

    Initial gitlist.org

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    $.getJSON('https://api.github.com/repos/klaussilveira/gitlist/contributors', function(data) {
+        var items = [];
+
+        $.each(data, function(key, val) {
+            items.push('<li><a href="' + val.url + '" rel="avatarover" data-placement="top" data-title="' + val.login + '" data-content="' + val.login + ' has made ' + val.contributions + ' contributions to GitList"><img src="' + val.avatar_url + '" width="32" height="32" /></a></li>');
+        });
+
+        $('<ul/>', {
+            'class': 'contributor-list',
+            html: items.join('')
+        }).appendTo('#contributors');
+        $('[rel=avatarover]').popover();
+    });
+
+    $('[rel=carousel]').carousel();
+    $('[rel=tooltip]').tooltip();
+    $('[rel=popover]').popover();
+    $('.feature').each(function() {
+        $(this).css('top',$(this).data('top')).css('left',$(this).data('left'));
+    });
+});
