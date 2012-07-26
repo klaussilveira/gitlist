92204a18f5d1148ebc05d4baa332bd8c259b877a
Grégoire Pineau <lyrixx@lyrixx.info>
2012-07-26 17:50:47 +0200

    Moved index.php to web/ folder
    And moved boot.php into src/ folder. Now all php code are in src/ folder

--- src/boot.php
+++ src/boot.php
@@ -1,7 +1,7 @@
 <?php
 
 // Load configuration
-$config = new GitList\Config('config.ini');
+$config = new GitList\Config(__DIR__.'/config.ini');
 $config->set('git', 'repositories', rtrim($config->get('git', 'repositories'), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR);
 
 // Startup and configure Silex application
---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+* GitList 0.3
+* https://github.com/klaussilveira/gitlist
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+$app = require __DIR__.'/../boot.php';
+$app->run();
