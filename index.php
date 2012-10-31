2abf80e333ba7fee1e220a47d375d8e681d8fed6
Klaus Silveira <contact@klaussilveira.com>
2012-10-30 23:57:47 -0200

    Setting the default date timezone to make sure systems without a properly configured php.ini can run GitList

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
  * https://github.com/klaussilveira/gitlist
  */
 
+// Set the default timezone for systems without date.timezone set in php.ini
+if (!ini_get('date.timezone')) {
+    date_default_timezone_set('UTC');
+}
+
 require 'vendor/autoload.php';
 $app = require 'boot.php';
 $app->run();
