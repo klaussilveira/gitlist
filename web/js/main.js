cb71550e7e698fb7cfa42ca6a5548834e14ada3a
Klaus Silveira <contact@klaussilveira.com>
2013-03-28 23:09:05 -0300

    Fixes #255

--- web/js/main.js
+++ web/js/main.js
@@ -27,15 +27,16 @@ $(function () {
 
     function paginate() {
         var $pager = $('.pager');
+
         $pager.find('.next a').one('click', function (e) {
             e.preventDefault();
-            $(this).css('pointer-events', 'none');
             $.get(this.href, function (html) {
                 $pager.after(html);
                 $pager.remove();
                 paginate();
             });
         });
+
         $pager.find('.previous').remove();
     }
     paginate();
