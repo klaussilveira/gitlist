89bc96b4e0845f5016fc6fed44d72692fe6db79a
Klaus Silveira <contact@klaussilveira.com>
2013-05-05 22:43:41 -0300

    Minor fixes and path checks

--- src/GitList/Application.php
+++ src/GitList/Application.php
@@ -15,6 +15,8 @@ use GitList\Provider\RoutingUtilServiceProvider;
  */
 class Application extends SilexApplication
 {
+    protected $path;
+
     /**
      * Constructor initialize services.
      *
@@ -24,43 +26,34 @@ class Application extends SilexApplication
     public function __construct(Config $config, $root = null)
     {
         parent::__construct();
-
         $app = $this;
-        $root = realpath($root);
+        $this->path = realpath($root);
 
         $this['debug'] = $config->get('app', 'debug');
         $this['filetypes'] = $config->getSection('filetypes');
-        $this['cache.archives'] = $root . DIRECTORY_SEPARATOR
-                . 'cache' . DIRECTORY_SEPARATOR . 'archives';
+        $this['cache.archives'] = $this->getCachePath() . 'archives';
 
         // Register services
         $this->register(new TwigServiceProvider(), array(
-            'twig.path'       => $root . DIRECTORY_SEPARATOR . 'views',
-            'twig.options'    => array('cache' => $root . DIRECTORY_SEPARATOR
-                                 . 'cache' . DIRECTORY_SEPARATOR . 'views'),
+            'twig.path'       => $this->getViewPath(),
+            'twig.options'    => array('cache' => $this->getCachePath() . 'views'),
         ));
 
         $repositories = $config->get('git', 'repositories');
-
-        $cached_repos = $config->get('app', 'cached_repos');
-        if (false === $cached_repos || empty($cached_repos)) {
-            $cached_repos = $root . DIRECTORY_SEPARATOR . 'cache'
-                    . DIRECTORY_SEPARATOR . 'repos.json';
+        $repositoryCache = $config->get('app', 'cached_repos');
+        if (false === $repositoryCache || empty($repositoryCache)) {
+            $repositoryCache = $this->getCachePath() . 'repos.json';
         }
 
         $this->register(new GitServiceProvider(), array(
             'git.client'      => $config->get('git', 'client'),
             'git.repos'       => $repositories,
-            'cache.repos'     => $cached_repos,
+            'cache.repos'     => $repositoryCache,
             'ini.file'        => "config.ini",
             'git.hidden'      => $config->get('git', 'hidden') ?
                                  $config->get('git', 'hidden') : array(),
             'git.default_branch' => $config->get('git', 'default_branch') ? $config->get('git', 'default_branch') : 'master',
         ));
-
-        $cached_repos = $root . DIRECTORY_SEPARATOR .
-                'cache' . DIRECTORY_SEPARATOR . 'repos.json';
-
 
         $this->register(new ViewUtilServiceProvider());
         $this->register(new RepositoryUtilServiceProvider());
@@ -74,7 +67,6 @@ class Application extends SilexApplication
             return $twig;
         }));
 
-
         // Handle errors
         $this->error(function (\Exception $e, $code) use ($app) {
             if ($app['debug']) {
@@ -85,6 +77,27 @@ class Application extends SilexApplication
                 'message' => $e->getMessage(),
             ));
         });
+    }
+
+    public function getPath()
+    {
+        return $this->path . DIRECTORY_SEPARATOR;
+    }
+    
+    public function setPath($path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    public function getCachePath()
+    {
+        return $this->path . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR;
+    }
+
+    public function getViewPath()
+    {
+        return $this->path . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
     }
 }
 
