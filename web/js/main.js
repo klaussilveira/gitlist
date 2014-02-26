bab2f11fad28c6d7a4c79102a65bf0dae17bf0ae
Kyle Hornberg <kyle.hornberg@gmail.com>
2014-02-26 11:01:35 -0600

    Optimized CodeMirror highlighting

--- web/js/main.js
+++ web/js/main.js
@@ -4,33 +4,31 @@ $(function() {
     // Format files and search results using CodeMirror
     // keyword defined in search.twig
     if ($('.sourcecode').length) {
-        $('.sourcecode').each(function() {
-            var value = $(this).text(),
-                search = (typeof keyword === 'undefined') ? false : true,
-                mode = $(this).attr('language'),
-                pre = $(this).get(0),
-                line = ($(this).attr('firstLineNumber') == undefined) ? 1 : parseInt($(this).attr('firstLineNumber'), 10);
+        var search = (typeof keyword === 'undefined') ? false : true;
 
+        if (search) {
             // CodeMirror search highlighing
-
-            // this seems redundent, slow, and messy
-            if (search) {
-                CodeMirror.defineMode("highlightSearch", function(config, parserConfig) {
-                    var searchOverlay = {
-                        token: function(stream, state) {
-                            if (stream.match(keyword)) {
-                                return "highlightSearch";
-                            }
-
-                            while (stream.next() != null && !stream.match(keyword, false)) {}
-                            return null;
+            CodeMirror.defineMode("highlightSearch", function(config, parserConfig) {
+                var searchOverlay = {
+                    token: function(stream, state) {
+                        if (stream.match(keyword)) {
+                            return "highlightSearch";
                         }
-                    };
-                    return CodeMirror.overlayMode(CodeMirror.getMode(config, mode), searchOverlay, true);
-                });
-            }
 
-            var viewer = CodeMirror(function(elt) {
+                        while (stream.next() != null && !stream.match(keyword, false)) {}
+                        return null;
+                    }
+                };
+                return CodeMirror.overlayMode(CodeMirror.getMode(config, parserConfig.inner), searchOverlay, true);
+            });
+         }
+
+        $('.sourcecode').each(function() {
+            var value  = $(this).text(),
+                mode   = $(this).attr('language'),
+                pre    = $(this).get(0),
+                line   = ($(this).attr('firstLineNumber') == undefined) ? 1 : Number($(this).attr('firstLineNumber')),
+                viewer = CodeMirror(function(elt) {
                 pre.parentNode.replaceChild(elt, pre);
             }, {
                 value: value,
@@ -39,24 +37,20 @@ $(function() {
                 matchBrackets: true,
                 lineWrapping: true,
                 readOnly: true,
-                mode: (search) ? 'highlightSearch' : mode,
+                mode: (search) ? {name: 'highlightSearch', inner: mode} : mode,
             });
         });
     }
 
     // New line formatter
-
     function lineFormater() {
         if ($('.CodeMirror-linenumber.CodeMirror-gutter-elt').length) {
             $('.CodeMirror-linenumber.CodeMirror-gutter-elt').each(function() {
-                var ln = $(this).text();
-                var file =
-                    $(this).html('<a href="#' + ln + '">' + ln + '</a>');
+                var ln   = $(this).text(),
+                    file = $(this).html('<a href="#' + ln + '">' + ln + '</a>');
             });
         }
-    }
-
-    // Run once when the page loads
+    } // Run once when the page loads
     lineFormater();
 
     if ($('#md-content').length) {
