62ae6e938db8a0c2ab56633d14562bd30c223587
Mike Gelfand <mikedld@mikedld.com>
2014-07-25 09:14:22 +0300

    Support hunk headers with no line counts
    As Wikipedia states, "In many versions of GNU diff, each range can omit
    the comma and trailing value s, in which case s defaults to 1".
    Change regex to make line count optional (we don't use it anyway).

--- src/GitList/Git/Repository.php
+++ src/GitList/Git/Repository.php
@@ -169,7 +169,7 @@ class Repository extends BaseRepository
                 switch ($log[0]) {
                     case "@":
                         // Set the line numbers
-                        preg_match('/@@ -([0-9]+),[0-9]+ \+([0-9]+)/', $log, $matches);
+                        preg_match('/@@ -([0-9]+)(?:,[0-9]+)? \+([0-9]+)/', $log, $matches);
                         $lineNumOld = $matches[1] - 1;
                         $lineNumNew = $matches[2] - 1;
                         break;
